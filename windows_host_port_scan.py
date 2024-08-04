ffb03708b240f5787ac0f1da16f1a89719f6740f
Aritra <aritrakunda3@gmail.com>
2024-08-04 12:11:15 +0530

    committed w.r.t socket

--- windows_host_port_scan.py
+++ windows_host_port_scan.py
@@ -1,28 +1,20 @@
-import subprocess
-import re
-from enum import Enum
-class Ports(Enum):
-    FTP = ":21"
-    TELNET = ":23"
-    SMB_DIRECT=":445"
-    SMB_INDIRECT_1=":137"
-    SMB_INDIRECT_2=":139" 
-final_open_ports=[]
-def search_open_ports():
-    ports=[Ports['FTP'].value,Ports['TELNET'].value,Ports['SMB_DIRECT'].value,Ports['SMB_INDIRECT_1'].value,Ports['SMB_INDIRECT_2'].value]
+import socket
+import sys
+
+ports = [23,21,137,139,445]
+
+if(len(sys.argv)==1):
+    print('No argumenets provided')
+else:    
+    ip=sys.argv[1] 
     for i in range(0,len(ports)):
-        print('Searching for port',ports[i])
-        first = subprocess.run(['netstat', '-aon'],capture_output=True)
-        full_command = subprocess.run(['findstr',ports[i]],input=first.stdout,capture_output=True)
-        output = full_command.stdout.decode()
-        pids = re.findall("[1-9]{1,}\r\n$",output)
-        if(len(pids)!=0):
-            pid_string=str(pids[0]).split("\r\n")
-            final_pid_string="\\<"+pid_string[0]+"\\>"
-            first = subprocess.run(['tasklist','/svc'],capture_output=True)
-            full_command = subprocess.run(['findstr',final_pid_string],input=first.stdout,capture_output=True)
-            output = full_command.stdout.decode()
-            print('port',ports[i],'is open due to',output, 'is running')
-            final_open_ports.append(Ports(ports[i]).name+":"+ports[i])
-search_open_ports()                
-print(final_open_ports)
+        try:
+            s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            s.connect((ip,ports[i]))
+            print('port',ports[i],'is open')
+            s.close()
+        except ConnectionRefusedError:
+            print('port',ports[i],'is closed, hence connection refused')
+        except PermissionError:
+            print('port is open however permission is denied')        
+           
