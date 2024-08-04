ced9e239f26ac6e3051195e0206bbe4d5077d5fa
Aritra <aritrakunda3@gmail.com>
2024-08-04 12:21:55 +0530

    renamed python script

---
+++ port_scan.py
@@ -0,0 +1,20 @@
+import socket
+import sys
+
+ports = [23,21,137,139,445]
+
+if(len(sys.argv)==1):
+    print('No argumenets provided')
+else:    
+    ip=sys.argv[1] 
+    for i in range(0,len(ports)):
+        try:
+            s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            s.connect((ip,ports[i]))
+            print('port',ports[i],'is open')
+            s.close()
+        except ConnectionRefusedError:
+            print('port',ports[i],'is closed, hence connection refused')
+        except PermissionError:
+            print('port',ports[i] ,'is open however permission is denied w.r.t socket')        
+           
